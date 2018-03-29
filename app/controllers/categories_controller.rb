1a90d4bdcf6428216961dde20b294bf14fda0961
Meng <jinmeng0522@gmail.com>
2018-03-29 04:19:04 +0000

    temp

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,11 @@
 class CategoriesController < GroupingController
   # index - inherited
   # show - inherited
+  def new_or_edit
+    @categories = Category.find(:all)
+    @category = Category.find_by_id(params[:id])
+    #@category.attributes = params[:category]
+    if request.post?
+    end
+  end
 end
